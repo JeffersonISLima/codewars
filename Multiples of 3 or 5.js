5c903a32715802e2260afe4796069466842f6aa5
Jefferson Lima <jeffersoninacio@hotmail.com>
2019-12-07 13:10:38 -0300

    Multiples of 3 or 5

--- Multiples of 3 or 5.js
+++ Multiples of 3 or 5.js
@@ -1,4 +1,5 @@
-/* https://www.codewars.com/kata/514b92a657cdc65150000006
+/*
+ https://www.codewars.com/kata/514b92a657cdc65150000006
 
 Multiples of 3 or 5
 
@@ -7,11 +8,10 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
 
- */
-
-/* Named function solution
-function solution(number) {
+// My solution
+const solution = number => {
   let sum = 0;
   for (let i = 0; i < number; i += 1) {
     if (i % 3 === 0 || i % 5 === 0) {
@@ -19,20 +19,15 @@ function solution(number) {
     }
   }
   return sum;
-}
-console.log(solution(10)); */
-
-// Arrow function solution
-const solution = ((number) =>{
-  let sum = 0;
-  for (let i = 0; i < number; i += 1) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  return sum;
-});
+};
 console.log(solution(10));
 
+// Other solution
+function solution(number) {
+  var n3 = Math.floor(--number / 3),
+    n5 = Math.floor(number / 5),
+    n15 = Math.floor(number / 15);
+  return (3 * n3 * (n3 + 1) + 5 * n5 * (n5 + 1) - 15 * n15 * (n15 + 1)) / 2;
+}
 
-// DONE - KATA delivered - Job Finish
+// Done
